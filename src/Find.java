952c2f7ee82b5ccde6b602321be37708420dc93f
Сибагатулин_Азат <Sibagatulin.azat@yandex.ru>
2018-05-03 02:04:48 +0500

    path

--- src/Find.java
+++ src/Find.java
@@ -1,68 +1,67 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import static java.io.File.separator;
 
 public class Find {
 
-    static String findWithoutDir(String file) throws IOException {
-        String result = "File not found";
-        File f = new File(file);
-        if (f.exists()) return convert(f);
-        return result;
+    static List<File> findWithoutDir(String file) throws IOException {
+        File parent = new File(".");
+        return convert(find(parent, file));
     }
 
-    static String findWithoutDirR(String file) throws IOException {
-        File f = new File(file);
-        if (f.exists()) return convert(f);
-        else {
-            File parent = new File(".");
-            return findInside(parent, file);
-        }
+    static List<File> findWithoutDirR(String file) throws IOException {
+        File parent = new File(".");
+        return convert(findInside(parent, file));
     }
 
-    static String find(File dir, String file) throws IOException {
-        String result = "File not found";
+    static List<File> find(File dir, String file) throws IOException {
+        List<File> result = new ArrayList<>();
         String[] dirNameFiles = dir.list();
         File[] dirFiles = dir.listFiles();
         if (dirFiles != null && dirNameFiles != null) {
             for (int i=0; i<dirFiles.length; i++) {
                 if (Objects.equals(dirNameFiles[i], file)) {
-                    return convert(dirFiles[i]);
+                    result.add(dirFiles[i]);
                 }
             }
         }
         return result;
     }
 
-    static String findInside(File dir, String file) throws IOException {
-        String result = "File not found";
+    static List<File> findInside(File dir, String file) throws IOException {
+        List<File> result = new ArrayList<>();
         String[] dirNameFiles = dir.list();
         File[] dirFiles = dir.listFiles();
         if (dirFiles != null && dirNameFiles != null) {
             for (int i=0; i<dirFiles.length; i++) {
                 if (Objects.equals(dirNameFiles[i], file)) {
-                    return convert(dirFiles[i]);
+                    result.add(dirFiles[i]);
                 }
                 if (dirFiles[i].isDirectory()) {
-                    result = findInside(dirFiles[i], file);
-                    if (!Objects.equals(result, "File not found")) return result;
+                    result.addAll(findInside(dirFiles[i], file));
                 }
             }
         }
         return result;
     }
 
-    private static String convert(File file) {
-        StringBuilder path = new StringBuilder(file.getAbsolutePath());
-        String[] parts = path.toString().split(separator + separator);
-        path = new StringBuilder(parts[0]);
-        for (int i = 1; i < parts.length; i++) {
-            if (Objects.equals(parts[i], ".")) continue;
-            path.append(separator).append(parts[i]);
+    static List<File> convert(List<File> list) {
+        List<File> result = new ArrayList<>();
+        for (File element: list) {
+            StringBuilder path = new StringBuilder(element.getPath());
+            String[] parts = path.toString().split(separator + separator);
+            path = new StringBuilder();
+            for (int i = 0; i < parts.length - 1; i++) {
+                if (Objects.equals(parts[i], ".")) continue;
+                path.append(parts[i]).append(separator);
+            }
+            path.append(parts[parts.length-1]);
+            result.add(new File(path.toString()));
         }
-        return path.toString();
+        return result;
     }
-
 }
--- tests/FindTest.java
+++ tests/FindTest.java
@@ -2,48 +2,91 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import static java.io.File.separator;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FindTest {
 
-    private String notFound = "File not found";
+    private List<File> empty = new ArrayList<>();
+    private List<File> result = new ArrayList<>();
+    private File file = new File("Text.txt");
+    private File file1 = new File("files"+separator+"Text1.txt");
+    private File file2 = new File("files"+separator+"file2"+separator+"Text2.txt");
+    private File file3_1 = new File("files"+separator+"file2"+separator+"Text3.txt");
+    private File file3_2 = new File("files"+separator+"file3"+separator+"Text3.txt");
+    private File file4 = new File("files"+separator+"file2"+separator+"file4"+separator+"Text4.txt");
+    private File file5 = new File("files"+separator+"file2"+separator+"file4"+separator+"file5"+separator+"Text5.txt");
     private File dir = new File("files");
-    private File dir1 = new File("files\\file1");
-    private File dir2 = new File("files\\file2");
-    private File dir4 = new File("files\\file2\\file4");
+    private File dir1 = new File("files"+separator+"file1");
+    private File dir2 = new File("files"+separator+"file2");
+    private File dir4 = new File("files"+separator+"file2"+separator+"file4");
 
     @Test
     void findInside() throws IOException {
-        assertEquals(Find.findInside(dir, "Text1.txt"), "D:\\FindDir\\files\\Text1.txt");
-        assertEquals(Find.findInside(dir, "Text5.txt"), "D:\\FindDir\\files\\file2\\file4\\file5\\Text5.txt");
-        assertEquals(Find.findInside(dir1, "Text1.txt"), notFound);
-        assertEquals(Find.findInside(dir2, "Text3.txt"), notFound);
-        assertEquals(Find.findInside(dir4, "Text4.txt"), "D:\\FindDir\\files\\file2\\file4\\Text4.txt");
+        result.add(file1);
+        assertEquals(Find.findInside(dir, "Text1.txt"), result);
+        result.remove(file1);
+        result.add(file5);
+        assertEquals(Find.findInside(dir, "Text5.txt"), result);
+        result.remove(file5);
+        assertEquals(Find.findInside(dir1, "Text1.txt"), empty);
+        assertEquals(Find.findInside(dir1, "Text3.txt"), empty);
+        result.add(file4);
+        assertEquals(Find.findInside(dir4, "Text4.txt"), result);
+        result.remove(file4);
+        result.add(file3_1);
+        result.add(file3_2);
+        assertEquals(Find.findInside(dir, "Text3.txt"), result);
+        result.remove(file3_1);
+        result.remove(file3_2);
     }
 
     @Test
     void find() throws IOException {
-        assertEquals(Find.find(dir, "Text1.txt"), "D:\\FindDir\\files\\Text1.txt");
-        assertEquals(Find.find(dir, "Text5.txt"), notFound);
-        assertEquals(Find.find(dir1, "Text1.txt"), notFound);
-        assertEquals(Find.find(dir2, "Text2.txt"), "D:\\FindDir\\files\\file2\\Text2.txt");
-        assertEquals(Find.find(dir4, "Text4.txt"), "D:\\FindDir\\files\\file2\\file4\\Text4.txt");
+        result.add(file1);
+        assertEquals(Find.find(dir, "Text1.txt"), result);
+        result.remove(file1);
+        result.add(file5);
+        assertEquals(Find.find(dir, "Text5.txt"), empty);
+        result.remove(file5);
+        result.add(file1);
+        assertEquals(Find.find(dir1, "Text1.txt"), empty);
+        result.remove(file1);
+        result.add(file2);
+        assertEquals(Find.find(dir2, "Text2.txt"), result);
+        result.remove(file2);
+        result.add(file4);
+        assertEquals(Find.find(dir4, "Text4.txt"), result);
+        result.remove(file4);
     }
 
     @Test
     void findWithoutDirR() throws IOException {
-        assertEquals(Find.findWithoutDirR("Text.txt"), "D:\\FindDir\\Text.txt");
-        assertEquals(Find.findWithoutDirR("Text5.txt"), "D:\\FindDir\\files\\file2\\file4\\file5\\Text5.txt");
-        assertEquals(Find.findWithoutDirR("Text1.txt"), "D:\\FindDir\\files\\Text1.txt");
-        assertEquals(Find.findWithoutDirR("Text123.txt"), notFound);
+        result.add(file);
+        assertEquals(Find.findWithoutDirR("Text.txt"), result);
+        result.remove(file);
+        result.add(file5);
+        assertEquals(Find.findWithoutDirR("Text5.txt"), result);
+        result.remove(file5);
+        result.add(file1);
+        assertEquals(Find.findWithoutDirR("Text1.txt"), result);
+        result.remove(file1);
+        assertEquals(Find.findWithoutDirR("Text123.txt"), empty);
+        result.add(file3_1);
+        result.add(file3_2);
+        assertEquals(Find.findWithoutDirR("Text3.txt"), result);
+        result.add(file3_1);
+        result.add(file3_2);
     }
 
     @Test
     void findWithoutDir() throws IOException {
-        assertEquals(Find.findWithoutDir("Text.txt"), "D:\\FindDir\\Text.txt");
-        assertEquals(Find.findWithoutDir("Text1.txt"), notFound);
-
+        result.add(file);
+        assertEquals(Find.findWithoutDir("Text.txt"), result);
+        result.remove(file);
+        assertEquals(Find.findWithoutDir("Text1.txt"), empty);
     }
-
 }
